0f6f4beb394753a32f0284a8432049be601d4176
sean-one <basement365@gmail.com>
2018-11-09 09:06:40 -0800

    working on get request for projects and actions

--- api/server.js
+++ api/server.js
@@ -1,7 +1,49 @@
 const express = require('express')
+const actionDb = require('../data/helpers/actionModel.js');
+const projectDb = require('../data/helpers/projectModel.js');
 
 const server = express();
 
+server.use(express.json());
 
+//actions
+server.get('/api/actions', async (req, res) => {
+    try {
+        const actions = await actionDb.get()
+        res.json({ actions })
+    } catch (error) {
+        console.log(error)
+    }
+})
+
+server.get('/api/actions/:id', async (req, res) => {
+    const id = req.params.id;
+    try {
+        const action = actionDb.get(id);
+        res.json({ action })
+    } catch (error) {
+        console.log(error)
+    }
+})
+
+//projects
+server.get('/api/projects', async (req, res) => {
+    try {
+        const projects = await projectDb.get()
+        res.json({ projects })
+    } catch (error) {
+        console.log(error)
+    }
+})
+
+server.get('/api/projects/:id', async (req, res) => {
+    const id = req.params.id;
+    try {
+        const project = await projectDb.get(id);
+        res.json({ project })
+    } catch (error) {
+        console.log(error)
+    }
+})
 
 module.exports = server;
--- index.js
+++ index.js
@@ -1,5 +1,7 @@
+// https://github.com/LambdaSchool/Sprint-Challenge-Node-Express/pull/435
+
 const server = require('./api/server.js');
 
 const port = 7500;
 
-server.listen(port, console.log('You are not alone...'));
+server.listen(port, console.log(`\n${port} You are not alone...`));
